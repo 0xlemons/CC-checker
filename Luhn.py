cde1d3aa2b4d757fa8db77743c812af1ca23200d
Lemons <159881372+0xlemons@users.noreply.github.com>
2024-04-22 18:56:01 -0700

    Update Luhn.py

--- Luhn.py
+++ Luhn.py
@@ -1,35 +1,16 @@
 class Luhn:
-    def check(cc_number):
-        
-        weight = "2121212121212121"
-        values = []
-        index = 0
-        for number in cc_number:
-            num = int(weight[index])
-            index += 1
-            product = int(num) * int(number)
-            if product >= 10:
-                product = str(product)
-                product = int(product[0]) + int(product[1])
-                if product >= 10:
-                    return False
-            digit = str(product)
-            if digit[0] == '4':
-                brand = 'Visa'
-            elif digit[:3] == '34' or digit[:3] == '37':
-                brand = 'American Express'
-            elif digit[:4] == '6011' or digit[:2] == '65' or (digit[:6] >= '622126' and digit[:6] <= '622925') or (digit[:3] >=  '644' and digit[:3] <= '649'):
-                brand = 'Discover Card'
-            elif digit[:2] >= '51' and digit[:2] <= '55':
-                brand = 'Mastercard'
-            else:
-                brand = 'Unkown Brand'
-            values.append(f'{product} : {brand}')
-        total = 0
-        for val in values:
-            total += val
-        total = str(total)
-        if int(total[len(total) - 1]) % 10 == 0:
-
-            return True
-        return False
+  @staticmethod
+  def check(cc_number):
+      weight = "2121212121212121"  
+      while len(weight) < len(cc_number):
+          weight += weight  
+      values = []
+      for i, number in enumerate(cc_number):
+          num = int(weight[i])
+          product = num * int(number)
+          if product >= 10:
+              product = sum(int(digit) for digit in str(product))
+          values.append(product)
+      total = sum(values)
+      
+      return total % 10 == 0
