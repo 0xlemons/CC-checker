0672421e72138129cc9bb6052eddd8610d93ea2a
Lemons <159881372+0xlemons@users.noreply.github.com>
2024-04-22 18:55:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 from Luhn import Luhn
 import random, time, threading
+
 bins = []
-numbers = [1,2,3,4,5,6,7,8,9]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 file = open('bins.txt', 'r+')
 data = file.readlines()
 for bin in data:
-    bins.append(bin)
+    bins.append(bin.strip())  # Remove newline characters
 file.close()
+
 def main():
     cc_number = ""
     bin = random.choice(bins)
     cc_number += bin
-    for i in range(10):
+    for _ in range(10):
         l = str(random.choice(numbers))
         cc_number += l
-    
-    result = Luhn.check(cc_number)
-    if result == True:
+
+    result= Luhn.check(cc_number)
+    if result:
+        digit = str(cc_number)
+        if digit[0] == '4':
+            brand = 'Visa'
+        elif digit[:3] == '34' or digit[:3] == '37':
+            brand = 'American Express'
+        elif digit[:4] == '6011' or digit[:2] == '65' or (digit[:6] >= '622126' and digit[:6] <= '622925') or (digit[:3] >=  '644' and digit[:3] <= '649'):
+            brand = 'Discover Card'
+        elif digit[:2] >= '51' and digit[:2] <= '55':
+            brand = 'Mastercard'
+        else:
+            brand = 'Unkown Brand'
         print(f"[+]: Valid Credit Card Number ({cc_number})")
         with open("valid.txt", 'a+') as f:
-            f.write(f"{cc_number}\n")
+            f.write(f"{cc_number} : {brand}\n")
     else:
         print(f"[-]: Invalid Credit Card Number ({cc_number})")
     time.sleep(random.uniform(0.1, 0.2))
     return main()
 
 if __name__ == "__main__":
-    for i in range(int(input("Enter amount of threads: "))):
+    for _ in range(int(input("Enter amount of threads: "))):
         threading.Thread(target=main).start()
